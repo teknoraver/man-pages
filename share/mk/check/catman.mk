5a34548ecbf4200b4a7bcc3ce9f26bedd84450cb
Alejandro Colomar <alx@kernel.org>
2023-04-22 20:55:21 +0200

    *.mk: check-catman: Support mdoc(7) pages
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- share/mk/check/catman.mk
+++ share/mk/check/catman.mk
@@ -23,8 +23,8 @@ COLFLAGS         := $(DEFAULT_COLFLAGS) $(EXTRA_COLFLAGS)
 COL              := col
 
 
-_CHECK_catman_grep := $(patsubst $(MANDIR)/%,$(_MANDIR)/%.cat.grep,$(NONSO_MAN))
-_CHECK_catman := $(patsubst $(MANDIR)/%,$(_MANDIR)/%.check-catman.touch,$(NONSO_MAN))
+_CHECK_catman_grep := $(patsubst $(MANDIR)/%,$(_MANDIR)/%.cat.grep,$(NONSO_MAN) $(NONSO_MDOC))
+_CHECK_catman := $(patsubst $(MANDIR)/%,$(_MANDIR)/%.check-catman.touch,$(NONSO_MAN) $(NONSO_MDOC))
 
 
 $(_CHECK_catman_grep): %.grep: % | $$(@D)/
