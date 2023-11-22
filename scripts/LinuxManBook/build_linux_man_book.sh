9cf69afc926e009156eeb15314643a97148fdcb2
Alejandro Colomar <alx@kernel.org>
2023-11-22 15:36:25 +0100

    scripts/LinuxManBook/build_linux_man_book.sh: Allow running from any working directory
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- scripts/LinuxManBook/build_linux_man_book.sh
+++ scripts/LinuxManBook/build_linux_man_book.sh
@@ -3,18 +3,20 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 (
-	./prepare_linux_man_book.pl "$1" \
+	"$(dirname "$0")"/prepare_linux_man_book.pl "$1" \
 	| groff -z -dPDF.EXPORT=1 -dLABEL.REFS=1 -dpaper=a4 -Tpdf -k -pet \
-		-M. -F. -mandoc -manmark -P-pa4 \-rC1 -rCHECKSTYLE=3 2>&1 \
+		-M"$(dirname "$0")" -mandoc -manmark \
+		-F"$(dirname "$0")" -P-pa4 \-rC1 -rCHECKSTYLE=3 2>&1 \
 	| LC_ALL=C grep '^\. *ds ';
 
-	./prepare_linux_man_book.pl "$1";
+	"$(dirname "$0")"/prepare_linux_man_book.pl "$1";
 ) \
 | preconv \
 | tbl \
 | eqn -Tpdf \
 | (
-	troff -Tpdf -ms <LMBfront.ms;
-	troff -Tpdf -M. -F. -mandoc -manmark -dpaper=a4;
+	troff -Tpdf -ms <"$(dirname "$0")"/LMBfront.ms;
+	troff -Tpdf -M"$(dirname "$0")" -mandoc -manmark \
+		-F"$(dirname "$0")" -dpaper=a4;
 ) \
-| gropdf -F. -pa4;
+| gropdf -F"$(dirname "$0")" -pa4;
