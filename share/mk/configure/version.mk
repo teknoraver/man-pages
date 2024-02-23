d3a1482d59c855eabc5493a433c155126c15d4c1
Alejandro Colomar <alx@kernel.org>
2024-02-23 15:38:59 +0100

    share/mk/configure/version.mk: $DISTNAME: Pass --dirty to git-describe(1)
    This makes sure we don't accidentally produce release tarballs from a
    dirty repository.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- share/mk/configure/version.mk
+++ share/mk/configure/version.mk
@@ -12,7 +12,7 @@ include $(MAKEFILEDIR)/configure/build-depends/git.mk
 include $(MAKEFILEDIR)/configure/verbose.mk
 
 
-DISTNAME    := $(shell $(GIT) describe)
+DISTNAME    := $(shell $(GIT) describe --dirty)
 DISTVERSION := $(patsubst man-pages-%,%,$(DISTNAME))
 DISTDATE    := $(shell $(GIT) log -1 --format='%aD')
 
