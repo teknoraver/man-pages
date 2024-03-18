3016c2180bc64b4b573d3effc828490531762314
Alejandro Colomar <alx@kernel.org>
2024-03-19 00:54:15 +0100

    share/mk/: wsfix
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- share/mk/dist/files.mk
+++ share/mk/dist/files.mk
@@ -15,11 +15,11 @@ include $(MAKEFILEDIR)/configure/version.mk
 include $(MAKEFILEDIR)/dist/_.mk
 
 
-DISTFILES   := $(shell $(DISTFILESCMD) | $(SED) 's,:,\\:,g')
-_DISTFILES  := $(patsubst $(srcdir)/%,$(_DISTDIR)/%,$(DISTFILES))
-_DISTPAGES  := $(filter     $(_DISTDIR)/man%,$(_DISTFILES))
-_DISTVERSION:= $(_DISTDIR)/share/mk/configure/version.mk
-_DISTOTHERS := $(filter-out $(_DISTPAGES) $(_DISTVERSION), $(_DISTFILES))
+DISTFILES    := $(shell $(DISTFILESCMD) | $(SED) 's,:,\\:,g')
+_DISTFILES   := $(patsubst $(srcdir)/%,$(_DISTDIR)/%,$(DISTFILES))
+_DISTPAGES   := $(filter $(_DISTDIR)/man%,$(_DISTFILES))
+_DISTVERSION := $(_DISTDIR)/share/mk/configure/version.mk
+_DISTOTHERS  := $(filter-out $(_DISTPAGES) $(_DISTVERSION), $(_DISTFILES))
 
 
 FORCE_DISTVERSION := \
