b00b746f20133babc2f69cf1ae49e655c69e4fc5
Alejandro Colomar <alx@kernel.org>
2024-03-18 16:50:28 +0100

    share/mk/: Consistency fixes in copyright lines
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- share/mk/configure/build-depends/diffoscope.mk
+++ share/mk/configure/build-depends/diffoscope.mk
@@ -1,4 +1,4 @@
-# Copyright 2024 Alejandro Colomar <alx@kernel.org>
+# Copyright 2024, Alejandro Colomar <alx@kernel.org>
 # SPDX-License-Identifier: LGPL-3.0-only WITH LGPL-3.0-linking-exception
 
 
