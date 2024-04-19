6ffd1e71bd9a9bd9cc1c48a1c2edd1b8c34891fa
Alejandro Colomar <alx@kernel.org>
2024-04-19 15:16:34 +0200

    share/mk/: build-fonts-tinos: Build TinosR.afm from Tinos-Regular.ttf
    This will allow generating the font description file from it, which will
    be done in the next commit.
    
    Link: <https://technicallywewrite.com/2023/09/16/addfonts>
    Link: <https://www.schaffter.ca/mom/momdoc/appendices.html#fonts>
    Cc: Jan Eden <tech@eden.one>
    Cc: "G. Branden Robinson" <branden@debian.org>
    Cc: Deri James <deri@chuzzlewit.myzen.co.uk>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- share/mk/build/fonts/tinos.mk
+++ share/mk/build/fonts/tinos.mk
@@ -8,14 +8,17 @@ MAKEFILE_BUILD_FONTS_TINOS_INCLUDED := 1
 
 include $(MAKEFILEDIR)/build/fonts/_.mk
 include $(MAKEFILEDIR)/configure/build-depends/texlive-fonts-extra/Tinos.pfb.mk
+include $(MAKEFILEDIR)/configure/build-depends/texlive-fonts-extra-links/Tinos-Regular.ttf.mk
+include $(MAKEFILEDIR)/configure/build-depends/fontforge/fontforge.mk
 include $(MAKEFILEDIR)/configure/build-depends/groff/pfbtops.mk
 
 
 TINOR := $(MAKEFILEDIR)/build/fonts/devpdf/TINOR
 
 
-_TINOS_PFA := $(_FONTSDIR)/devpdf/Tinos.pfa
-_TINOR     := $(_FONTSDIR)/devpdf/TINOR
+_TINOS_PFA  := $(_FONTSDIR)/devpdf/Tinos.pfa
+_TINOSR_AFM := $(_FONTSDIR)/devpdf/TinosR.afm
+_TINOR      := $(_FONTSDIR)/devpdf/TINOR
 
 _TINOS := $(_TINOS_PFA) $(_TINOR)
 
@@ -24,7 +27,11 @@ $(_TINOS_PFA): $(TINOS_PFB) $(MK) | $$(@D)/
 	$(info	$(INFO_)PFBTOPS		$@)
 	$(PFBTOPS) <$< >$@
 
-$(_TINOR): $(_FONTSDIR)/%: $(MAKEFILEDIR)/build/fonts/% $(MK) | $$(@D)/
+$(_TINOSR_AFM): $(TINOSR_TTF) $(MK) | $$(@D)/
+	$(info	$(INFO_)FONTFORGE	$@)
+	$(FONTFORGE) $(FONTFORGEFLAGS) -lang=ff -c 'Open("$<");Generate("$@");'
+
+$(_TINOR): $(_FONTSDIR)/%: $(MAKEFILEDIR)/build/fonts/% $(_TINOSR_AFM) $(MK) | $$(@D)/
 	$(info	$(INFO_)CP		$@)
 	$(CP) -T $< $@
 
