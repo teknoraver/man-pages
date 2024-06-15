6fc32a0a6ae2cedf2e954b8b48cf8ba0d5ddf2a1
Alejandro Colomar <alx@kernel.org>
2024-06-15 18:32:32 +0200

    share/mk/: lint-man-so: Skip man.7
    We purposefully point to groff_man(7), which is not in this project.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- share/mk/lint/man/so.mk
+++ share/mk/lint/man/so.mk
@@ -6,14 +6,23 @@ ifndef MAKEFILE_LINT_MAN_SO_INCLUDED
 MAKEFILE_LINT_MAN_SO_INCLUDED := 1
 
 
+include $(MAKEFILEDIR)/build/_.mk
 include $(MAKEFILEDIR)/build/man/man.mk
 include $(MAKEFILEDIR)/configure/build-depends/coreutils/cut.mk
 include $(MAKEFILEDIR)/configure/build-depends/findutils/find.mk
 include $(MAKEFILEDIR)/configure/build-depends/findutils/xargs.mk
 include $(MAKEFILEDIR)/configure/build-depends/grep/grep.mk
+include $(MAKEFILEDIR)/configure/xfail.mk
+
+
+_XFAIL_LINT_man_so := \
+	$(_MANDIR)/man7/man.7.lint-man.so.touch
 
 
 _LINT_man_so := $(patsubst %, %.lint-man.so.touch, $(_SO_MAN))
+ifeq ($(SKIP_XFAIL),yes)
+_LINT_man_so := $(filter-out $(_XFAIL_LINT_man_so), $(_LINT_man_so))
+endif
 
 
 $(_LINT_man_so): %.lint-man.so.touch: % $(MK) | $$(@D)/
