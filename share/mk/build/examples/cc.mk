4ef1872d9f5a6fcc00feb44537bc21b1decac09e
Alejandro Colomar <alx@kernel.org>
2024-07-20 12:59:34 +0200

    share/mk/: Silence spurious warnings
    Those write(2) calls are unimportant, and we don't want to check errors.
    
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- share/mk/build/examples/cc.mk
+++ share/mk/build/examples/cc.mk
@@ -16,6 +16,7 @@ include $(MAKEFILEDIR)/configure/xfail.mk
 _XFAIL_EX_TU_o := \
 	$(_MANDIR)/man2/bpf.2.d/bpf.o \
 	$(_MANDIR)/man2/seccomp.2.d/seccomp.o \
+	$(_MANDIR)/man2/seccomp_unotify.2.d/seccomp_unotify.o \
 	$(_MANDIR)/man2/sigaction.2.d/sigaction.o \
 	$(_MANDIR)/man2/spu_run.2.d/spu_run.o \
 	$(_MANDIR)/man2/_syscall.2.d/_syscall.o \
@@ -31,6 +32,7 @@ _XFAIL_EX_TU_o := \
 	$(_MANDIR)/man3/__ppc_get_timebase.3.d/__ppc_get_timebase.o \
 	$(_MANDIR)/man3/pthread_getcpuclockid.3.d/pthread_getcpuclockid.o \
 	$(_MANDIR)/man3/rtime.3.d/rtime.o \
+	$(_MANDIR)/man3/sem_wait.3.d/sem_wait.o \
 	$(_MANDIR)/man3/setbuf.3.d/setbuf.o \
 	$(_MANDIR)/man3/stpncpy.3.d/stpncpy.o \
 	$(_MANDIR)/man3head/printf.h.3head.d/register_printf_specifier.o
