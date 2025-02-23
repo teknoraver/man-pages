21cf71044f79bafb1b3a1e16dd86008f0269a243
Alejandro Colomar <alx@kernel.org>
2025-02-23 18:52:19 +0100

    share/mk/: diffoscope(1) is in the diffoscope-minimal package
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- share/mk/dist/check/diffoscope.mk
+++ share/mk/dist/check/diffoscope.mk
@@ -7,7 +7,7 @@ MAKEFILE_DIST_CHECK_DIFFOSCOPE_INCLUDED := 1
 
 
 include $(MAKEFILEDIR)/configure/build-depends/coreutils/touch.mk
-include $(MAKEFILEDIR)/configure/build-depends/diffoscope/diffoscope.mk
+include $(MAKEFILEDIR)/configure/build-depends/diffoscope-minimal/diffoscope.mk
 include $(MAKEFILEDIR)/configure/directory_variables/build.mk
 include $(MAKEFILEDIR)/dist/check/dist.mk
 include $(MAKEFILEDIR)/dist/tar.mk
