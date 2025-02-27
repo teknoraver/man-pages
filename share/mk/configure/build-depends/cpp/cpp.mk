teknoraver/man-pages

commit 76f12e3fd3eab022f4f4d608c9a8e4d80cb81fd2

share/mk/: Escape '#' in regexes
GNU make(1) 4.2 seems to be interpreting those characters as the start
of a comment, so we need to escape them.  That seems to calm those old
versions of make(1), and doesn't affect negatively the newer ones, and
doesn't affect negatively grep(1) either.

Fixes: 35a780a99bd8 (2024-07-20; "share/mk/: CPPFLAGS: Only define _FORTIFY_SOURCE if it's not already defined")
Fixes: 2130162900ab (2024-11-03; "share/mk/, etc/shellcheck/: lint-sh: Add target to lint shell scripts")
Reported-by: Boris Pigin <boris.pigin@gmail.com>
Cc: Sam James <sam@gentoo.org>
Signed-off-by: Alejandro Colomar <alx@kernel.org>

share/mk/configure/build-depends/cpp/cpp.mk
@@ -19,7 +19,7 @@ CPP := $(CC) $(CFLAGS_) -E
 CPP_HAS_ALREADY_D_FORTIFY_SOURCE := \
 	$(shell \
 		$(CPP) -dM - -Wno-error </dev/null \
-		| $(GREP) '#define _FORTIFY_SOURCE ' >/dev/null \
+		| $(GREP) '\#define _FORTIFY_SOURCE ' >/dev/null \
 		&& $(ECHO) yes \
 		|| $(ECHO) no; \
 	)

share/mk/src/sh.mk
@@ -14,7 +14,7 @@ include $(MAKEFILEDIR)/configure/directory_variables/src.mk
 
 
 BIN_sh := $(shell $(FIND) $(SRCBINDIR) -type f \
-		| $(XARGS) $(GREP) -l '^#!/bin/\(sh\|bash\)\>' \
+		| $(XARGS) $(GREP) -l '^\#!/bin/\(sh\|bash\)\>' \
 		| $(SORT))
 
 

commit 46b7bcaae3cbd18b3022caa3ddd52e7be66bb21a

share/mk/: Don't pass an escaped # to grep(1)
We need to escape the # for old versions of make(1).  However, new
versions of grep(1) diagnose if it receives an escaped #.  To keep both
make(1) and grep(1) happy in both their old and new versions, we need to
take advantage of # not being a comment in bash(1) when not preceeded by
a space, and also of \# being translated into # by bash(1).

        alx@debian:~$ echo ''\#
        #
        alx@debian:~$ echo ''#
        #

Fixes: 76f12e3fd3ea (2025-02-10; "share/mk/: Escape '#' in regexes")
Reported-by: Sergei Trofimovich <slyich@gmail.com>
Cc: Florian Weimer <fw@deneb.enyo.de>
Cc: Boris Pigin <boris.pigin@gmail.com>
Suggested-by: Jakub Wilk <jwilk@jwilk.net>
References: <20250227162800.36exbwmqky6d7z4t@jwilk.net>
Signed-off-by: Alejandro Colomar <alx@kernel.org>

share/mk/configure/build-depends/cpp/cpp.mk
@@ -21,7 +21,7 @@ endif
 CPP_HAS_ALREADY_D_FORTIFY_SOURCE := \
 	$(shell \
 		$(CPP) -dM - -Wno-error </dev/null \
-		| $(GREP) '\#define _FORTIFY_SOURCE ' >/dev/null \
+		| $(GREP) ''\#'define _FORTIFY_SOURCE ' >/dev/null \
 		&& $(ECHO) yes \
 		|| $(ECHO) no; \
 	)

share/mk/src/sh.mk
@@ -14,7 +14,7 @@ include $(MAKEFILEDIR)/configure/directory_variables/src.mk
 
 
 BIN_sh := $(shell $(FIND) $(SRCBINDIR) -type f \
-		| $(XARGS) $(GREP) -l '^\#!/bin/\(sh\|bash\)\>' \
+		| $(XARGS) $(GREP) -l '^'\#'!/bin/\(sh\|bash\)\>' \
 		| $(SORT))
 
 